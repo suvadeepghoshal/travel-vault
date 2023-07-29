9797e5147447cb71a41a0fd9b6fd48fe523a40b9
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2023-07-29 14:14:23 +0530

    comm b/w ang and exp

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    fetch('http://localhost:3000/')
+      .then(response => {
+        return response.json() as Promise<any>;
+      })
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+}
--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,20 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  optionSuccessStatus: 200,
+};
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+
+// dummy to be removed later with the init route
 app.get('/', (_req, res) => {
   res.send({
     app: 'Travel Vault',
