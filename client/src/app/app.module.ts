c311354fa7d03ba8685ec5097ecc1dbd62971fac
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2023-07-30 20:34:34 +0530

    added header and footer basic ui, removed side nav

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,21 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderCmpComponent } from './header-cmp/header-cmp.component';
-import { SideNavCmpComponent } from './side-nav-cmp/side-nav-cmp.component';
 import { FooterCmpComponent } from './footer-cmp/footer-cmp.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderCmpComponent,
-    SideNavCmpComponent,
-    FooterCmpComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderCmpComponent, FooterCmpComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
