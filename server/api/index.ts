b0c7484e3d42a57a37e809a98393f16d6cf4e1c6
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2023-08-06 21:53:28 +0530

    moving to api folder for vercel deployment

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,11 +1,11 @@
 import bodyParser from 'body-parser';
-import express, { Response } from 'express';
+import express, { Express, Response } from 'express';
 import cors, { CorsOptions } from 'cors';
-import { CommonResponse } from './models/commonResponse';
+import { CommonResponse } from '../models/commonResponse';
 import { randomUUID } from 'crypto';
 
-const app = express();
-const PORT = 3000;
+const app: Express = express();
+const PORT: number = 3000;
 
 const corsOptions: CorsOptions = {
   origin: 'http://localhost:4200',
@@ -17,7 +17,9 @@ app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
 // dummy to be removed later with the init route
-app.get('/', (_req, res): Response<CommonResponse> => {
+app.get('/api', (_req, res): Response<CommonResponse> => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   return res.send({
     id: randomUUID(),
     date: Date(),
